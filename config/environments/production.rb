2dd36ee97e39042f6c7bef4ac1733b2ffedc4f2f
Kiminari Homma <u533u778@gmail.com>
2014-12-11 11:42:25 +0900

    changed plugin to maigun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-    ActionMailer::Base.smtp_settings = {
+  config.action_mailer.default_url_options = { host: 'callcloud.herokuapp.com' }
+  ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
